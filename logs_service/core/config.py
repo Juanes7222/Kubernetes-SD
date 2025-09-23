958f3c50b3e792ae7cd110222455f706bee27d79
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2025-09-22 23:00:52 -0500

    logger microservice added

--- logs_service/core/config.py
+++ logs_service/core/config.py
@@ -1,8 +1,16 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+ROOT_DIR = Path(__file__).parent.parent
+load_dotenv(ROOT_DIR / ".env")
 
 class Settings:
-    ENV: str = os.getenv("ENV", "development")
-    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
-    FIREBASE_CREDENTIALS: str = os.getenv("FIREBASE_CREDENTIALS", "../secrets/kubernetes-sd.json")
+    PROJECT_NAME: str = os.getenv("PROJECT_NAME", "Logs Service")
+    DESCRIPTION: str = os.getenv("DESCRIPTION", "Logging Microservice")
+    VERSION: str = os.getenv("VERSION", "2.0.0")
+
+    CORS_ORIGINS: list[str] = ["*"]  # Logs service accepts from everywhere
+    DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
 
 settings = Settings()
